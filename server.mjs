06b76d37f91fbaec1cd6fb86d6505ee826c7268c
Randal_Wang <randal_wang@resourcepro.com.cn>
2025-01-10 10:15:28 +0800

    chore: remove unused statement

--- server.mjs
+++ server.mjs
@@ -16,7 +16,7 @@ const handle = app.getRequestHandler();
 const BASE_DATA_DIR = join(process.cwd(), 'tiddlywiki-instances');
 const INSTANCES_FILE = join(BASE_DATA_DIR, 'instances.json');
 
-// Helper 函数：检查端口是否开放
+// Helper function: check if a port is open
 const isPortOpen = (port, host) => {
   return new Promise((resolve) => {
     const socket = new net.Socket();
@@ -38,6 +38,7 @@ const isPortOpen = (port, host) => {
 };
 
 // Helper 函数：等待端口开放
+// Helper function: wait for a port to open
 const waitForPort = async (port, host, retries = 10, delay = 1000) => {
   for (let i = 0; i < retries; i++) {
     const open = await isPortOpen(port, host);
@@ -73,8 +74,6 @@ const loadInstances = async () => {
         console.log(`TiddlyWiki instance started on port ${port}`);
       } else {
         console.error(`Failed to start TiddlyWiki instance on port ${port}`);
-        // 根据需求决定是否抛出错误或继续
-        // throw new Error(`TiddlyWiki instance failed to start on port ${port}`);
       }
     }
   }
@@ -87,7 +86,7 @@ app.prepare().then(async () => {
 
     const server = express();
 
-    // 处理所有其他路由
+    // Handle all other routes
     server.all('*', (req, res) => {
       return handle(req, res);
     });
@@ -98,41 +97,6 @@ app.prepare().then(async () => {
     });
   } catch (error) {
     console.error('Error starting server:', error);
-    process.exit(1); // 以错误代码退出
+    process.exit(1); // Exit with error
   }
-
-  // const instances = loadInstances();
-  //
-  // const server = express();
-  //
-  // // 反向代理 /wiki/:id/* 到对应的 TiddlyWiki 实例
-  // server.use('/wiki/:id', (req, res, nextFn) => {
-  //   const { id } = req.params;
-  //   if (instances?.[id]) {
-  //     console.log('instances id', instances?.[id]);
-  //     const proxy = createProxyMiddleware({
-  //       target: `http://localhost:${instances[id].port}`,
-  //       changeOrigin: true,
-  //       pathRewrite: (path) => path.replace(`/wiki/${id}`, ''),
-  //       ws: true, // 如果 TiddlyWiki 使用 WebSockets
-  //       logLevel: 'debug', // 可选，调试用
-  //     });
-  //     console.log('proxy', proxy);
-  //     proxy(req, res, nextFn).then(r => {
-  //       console.log('proxy', r);
-  //     });
-  //   } else {
-  //       res.status(404).send('Wiki 实例未找到');
-  //   }
-  // });
-  //
-  // // 处理所有其他路由
-  // server.all('*', (req, res) => {
-  //   return handle(req, res);
-  // });
-  //
-  // server.listen(port, (err) => {
-  //   if (err) throw err;
-  //   console.log(`> Ready on http://localhost:${port}`);
-  // });
 });
