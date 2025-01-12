f8858e4c129815d3ba65ac3331ed0d96dcabd128
Randal <admin@tecfancy.com>
2025-01-12 22:47:33 +0800

    feat: support start multiple apps with pm2

--- scripts/test-pm2-start-app.mjs
+++ scripts/test-pm2-start-app.mjs
@@ -1,43 +1,63 @@
+import { promisify } from "util";
 import pm2 from "pm2";
 
 /**
- * Start the application with PM2
- * @param params
- * @param {string} params.appName - Name of the application
- * @param {string} params.script - Path to the script
- * @param {string[]} params.args - Arguments to pass to the script
- * @param {Object} params.env - Environment variables to set
+ * @typedef {Object} AppConfig
+ * @property {string} appName - Name of the application
+ * @property {string} script - Path to the script
+ * @property {string[]} args - Arguments to pass to the script
+ * @property {Object} env - Environment variables
+ * @property {string} [namespace='mws'] - Namespace for the application
  */
-const startApp = (params) => {
-    const appName = params.appName;
-    const script = params.script;
-    const args = params.args;
-    const env = params.env;
 
-    pm2.connect((err) => {
-        if (err) {
-            console.error('PM2 connection error:', err);
-            process.exit(2);
-        }
+/**
+ * Start multiple apps with PM2
+ * @param {AppConfig[]} apps - Array of application configurations
+ */
+const startApps = async (apps) => {
+    const pm2Connect = promisify(pm2.connect).bind(pm2);
+    const pm2Start = promisify(pm2.start).bind(pm2);
+    const pm2List = promisify(pm2.list).bind(pm2);
+    const pm2Restart = promisify(pm2.restart).bind(pm2);
+    const pm2Disconnect = promisify(pm2.disconnect).bind(pm2);
 
-        pm2.start({
-            name: appName,
-            script,
-            args,
-            env: { ...env, PORT: 4236 },
-            namespace: 'mws',
-        }, (err) => {
-            if (err) {
-                console.error('Start MWS (Multi Wiki Server) error:', err);
-                process.exit(2);
+    try {
+        // Connect to PM2
+        await pm2Connect();
+        console.log('Connected to PM2');
+
+        // Get the list of existing apps
+        const existingApps = await pm2List();
+
+        for (const app of apps) {
+            const { appName, script, args, env, namespace = 'mws' } = app;
+
+            // Check if the app is already running
+            const isAppRunning = existingApps.some((app) => app.name === appName && app.namespace === namespace);
+
+            if (isAppRunning) {
+                console.log(`${appName} is already running, attempting to restart...`);
+                await pm2Restart(appName, { namespace });
+                console.log(`${appName} restarted`);
+            } else {
+                await pm2Start({
+                    name: appName,
+                    script,
+                    args,
+                    env: { ...env, PORT: 4236 },
+                    namespace,
+                });
+                console.log(`Started ${appName} with PM2, visit http://localhost:${env.PORT}/`);
             }
-
-            console.log(`Started ${appName} with PM2, visit http://localhost:${env.PORT}/`);
-
-            // Disconnect from PM2
-            pm2.disconnect();
-        });
-    });
+        }
+    } catch (error) {
+        console.error(`Error starting apps: ${error.message}`);
+        process.exit(2);
+    } finally {
+        // Disconnect from PM2
+        await pm2Disconnect();
+        console.log('Disconnected from PM2');
+    }
 };
 
-export default startApp;
+export default startApps;
--- scripts/test-pm2-start.mjs
+++ scripts/test-pm2-start.mjs
@@ -1,17 +1,28 @@
-import startApp from "./test-pm2-start-app.mjs";
+import startApps from "./test-pm2-start-app.mjs";
 
 const NODE_ENV = process.env.NODE_ENV;
+const NAMESPACE = process.env.NAMESPACE;
 
 const mwsAppArgsMap = {
     development: 'dev',
     production: 'start',
 };
 
-startApp({
-    appName: 'MWS (Multi Wiki Server)',
-    script: 'node_modules/next/dist/bin/next',
-    args: [mwsAppArgsMap[NODE_ENV]],
-    env: {
-        NODE_ENV,
-    },
+startApps([
+    {
+        appName: 'MWS (Multi Wiki Server)',
+        script: 'next',
+        args: [mwsAppArgsMap[NODE_ENV]],
+        env: {
+            NODE_ENV,
+        },
+        namespace: NAMESPACE,
+    }
+]).then(() => {
+    console.log('Successfully started');
+}).catch((error) => {
+    console.error(`Error starting apps: ${error.message}`);
+    process.exit(1);
+}).finally(() => {
+    process.exit(0);
 });
